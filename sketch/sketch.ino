1693b42e49b26a556207804f6539e56014c5115d
Alexandr Skrezhendevskiy <alekskrzh@gmail.com>
2019-04-30 14:39:35 +0300

    fastPID

--- sketch/sketch.ino
+++ sketch/sketch.ino
@@ -1,9 +1,26 @@
-void setup() {
-    
 
+
+#include <FastPID.h>
+
+
+
+float Kp=0.1, Ki=0.5, Kd=0, Hz=10;
+int output_bits = 8;
+bool output_signed = false;
+
+FastPID myPID(Kp, Ki, Kd, Hz, output_bits, output_signed);
+
+void setup()
+{
+  
 }
 
-void loop() {
-    
-
+void loop()
+{
+  int setpoint = 512; 
+  int feedback = 512;
+  uint8_t output = myPID.step(setpoint, feedback);
 }
+   
+
+
